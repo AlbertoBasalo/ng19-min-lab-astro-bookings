56bba38da50113808404846284401a46977fda93
Alberto Basalo <albertobasalo@hotmail.com>
2024-11-28 10:19:01 +0100

    feat: launch details

--- src/app/routes/home/home.page.ts
+++ src/app/routes/home/home.page.ts
@@ -1,6 +1,8 @@
 import { DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LAUNCHES } from '../../shared/data/launches.data';
+import { LaunchDto } from '../../shared/models/launch.dto';
 
 /**
  * Home page component
@@ -29,26 +31,5 @@ export class HomePage {
   /**
    * Array of launches
    */
- protected launches = [
-   {
-     id: 'lnc-1',
-     agencyId: 'agc-1',
-     rocketId: 'rck-1',
-     date: new Date(2029, 5, 1),
-     mission: 'Moon Landing',
-     destination: 'Moon',
-     pricePerSeat: 100,
-     status: 'scheduled',
-   },
-    {
-      id: 'lnc-2',
-      agencyId: 'agc-1',
-      rocketId: 'rck-2',
-      date: new Date(2039, 6, 20),
-      mission: 'Mars Landing',
-      destination: 'Mars',
-      pricePerSeat: 200,
-      status: 'scheduled',
-    },
-  ];
+  protected launches: LaunchDto[] = LAUNCHES;
 }
--- src/app/routes/launches/launch-details/launch-details.page.ts
+++ src/app/routes/launches/launch-details/launch-details.page.ts
@@ -1,13 +1,31 @@
-import { ChangeDetectionStrategy, Component, input, InputSignal } from '@angular/core';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input, InputSignal, Signal } from '@angular/core';
+import { LAUNCHES } from '../../../shared/data/launches.data';
+import { ROCKETS } from '../../../shared/data/rockets.data';
+import { LaunchDto } from '../../../shared/models/launch.dto';
+import { RocketDto } from '../../../shared/models/rocket.dto';
+// ToDo: use aliases paths
+
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [DatePipe, CurrencyPipe],
   template: `
-    <p>
+    <h2>
       <!-- The launch id is used like any other signal -->
-      launch-details for <b>{{ id() }}</b>
-    </p>
+      {{ id() }}
+    </h2>
+    @if (launch()) {
+      <p>Launch details for <b>{{ launch()?.mission }}</b></p>
+      <p>Rocket: {{ rocket()?.name }}</p>
+      <p>Destination: {{ launch()?.destination }}</p>
+      <p>Date: {{ launch()?.date | date }}</p>
+      <p>Price per seat: {{ launch()?.pricePerSeat | currency }}</p>
+      <p>Status: {{ launch()?.status }}</p>
+      <p>Capacity: {{ rocket()?.capacity }}</p>
+    } @else {
+      <p>Launch not found</p>
+    }
   `,
   styles: ``
 })
@@ -18,4 +36,15 @@ export default class LaunchDetailsPage {
    * - Will be provided by the router
    */
   public readonly id: InputSignal<string> = input.required<string>();
+
+  /**
+   * The launch object computed from the `id` input signal
+   * - Warning: this works because the `find` method is **synchronous**
+   */
+  protected launch: Signal<LaunchDto | undefined> = 
+    computed(() => LAUNCHES.find((launch) => launch.id === this.id()));
+
+
+  protected rocket: Signal<RocketDto | undefined> = 
+    computed(() => ROCKETS.find((rocket) => rocket.id === this.launch()?.rocketId));
 }
