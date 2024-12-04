6cbe1cd7e070a68ae14b1d081273e570d14be13a
Alberto Basalo <albertobasalo@hotmail.com>
2024-12-04 12:07:55 +0100

    clean: remove static data

--- src/app/shared/api/launches.repository.ts
+++ src/app/shared/api/launches.repository.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { environment } from "@environments/environment";
-import { LaunchDto, NULL_LAUNCH } from "@models/launch.dto";
+import { LaunchDto } from "@models/launch.dto";
 import { Observable } from "rxjs";
-import { LAUNCHES } from "../data/launches.data";
 
 @Injectable({
   providedIn: 'root',
@@ -12,10 +11,7 @@ export class LaunchesRepository {
   private readonly http = inject(HttpClient);
   private readonly apiUrl = environment.apiUrl;
   private readonly launchesEndpoint = `${this.apiUrl}/launches`;
-  /**
-   * Gets all the launches
-   */
-  public getAll = (): LaunchDto[] => LAUNCHES;
+
 
   /**
    * Gets all the launches
@@ -23,13 +19,7 @@ export class LaunchesRepository {
    */
   public getAll$ = (): Observable<LaunchDto[]> => this.http.get<LaunchDto[]>(this.launchesEndpoint);
 
-  /**
-   * Gets a launch by id
-   * @param id - The launch id
-   */
-  public getById = (id: string): LaunchDto => LAUNCHES.find((launch) => launch.id === id) || NULL_LAUNCH;
-
-  /**
+   /**
    * Gets a launch by id
    * @param id - The launch id
    * @returns An observable of the launch with the given id
--- src/app/shared/api/rockets.repository.ts
+++ src/app/shared/api/rockets.repository.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { environment } from "@environments/environment";
-import { NULL_ROCKET, RocketDto } from "@models/rocket.dto";
+import { RocketDto } from "@models/rocket.dto";
 import { Observable } from "rxjs";
-import { ROCKETS } from "../data/rockets.data";
 
 @Injectable({
   providedIn: 'root',
@@ -12,10 +11,7 @@ export class RocketsRepository {
   private readonly http = inject(HttpClient);
   private readonly apiUrl = environment.apiUrl;
   private readonly rocketsEndpoint = `${this.apiUrl}/rockets`;
-  /**
-   * Gets all the rockets
-   */
-  public getAll = (): RocketDto[] => ROCKETS;
+
 
   /**
    * Gets all the rockets
@@ -23,13 +19,7 @@ export class RocketsRepository {
    */
   public getAll$ = (): Observable<RocketDto[]> => this.http.get<RocketDto[]>(this.rocketsEndpoint);
 
-  /**
-   * Gets a rocket by id
-   * @param id - The rocket id
-   */
-  public getById = (id: string): RocketDto => ROCKETS.find((rocket) => rocket.id === id) || NULL_ROCKET;
-
-  /**
+   /**
    * Gets a rocket by id
    * @param id - The rocket id
    * @returns An observable of the rocket with the given id
